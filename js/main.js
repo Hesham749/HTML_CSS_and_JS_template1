311b229d95341828871b5b43e4f49b11d8fb4402
Hesham749 <heesham749@gmail.com>
2024-01-19 03:36:57 +0200

    scroll-update-fade

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var toTop = document.querySelector('.to-top');
-onscroll=function(){
-    if(scrollY >= 750){
-        toTop.style.display='block';
-    }else{
-        toTop.style.display='none'
+var toTop = document.querySelector(".to-top");
+onscroll = function () {
+    if (scrollY >= 750) {
+        $(".to-top").fadeIn(300);
+    } else {
+        $(".to-top").fadeOut(300);
     }
-}
-toTop.onclick=function () {
-    window.scrollTo({top:0 , behavior:"smooth"});
-}
+};
+toTop.onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
